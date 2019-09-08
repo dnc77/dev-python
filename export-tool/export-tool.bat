4a41b173f1463debfdb820b7a7e92fe50872f44b
duncan <duncan@typhoon>
2019-09-08 13:35:49 +1000

    Delete old files

--- export-tool/export-tool.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if exist ..\plot-tool\local.db (
-   del ..\plot-tool\local.db
-)
-
-python datagather.py
-move /Y local.db ..\plot-tool\local.db
-echo "Database local.db created"
--- plot-tool/plot.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist local.db (
-   echo 'Needs local.db'
-   exit
-)
-
-python plot.py
